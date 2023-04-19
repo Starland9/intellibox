4a994d344dee9a86972dc9e3d671d0fdd19755f8
Landry Simo <86841267+LandPix200@users.noreply.github.com>
2023-04-19 17:29:37 +0100

    set async button

--- lib/modules/devinette/model.dart
+++ lib/modules/devinette/model.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:translator/translator.dart';
 
 part 'model.g.dart';
 
@@ -10,6 +11,14 @@ class Devinette {
 
   Devinette(this.title, this.question, this.answer);
 
-  factory Devinette.fromJson(Map<String, dynamic> json) => _$DevinetteFromJson(json);
+  factory Devinette.fromJson(Map<String, dynamic> json) =>
+      _$DevinetteFromJson(json);
   Map<String, dynamic> toJson() => _$DevinetteToJson(this);
+
+  static Future<Devinette> translated(Devinette d) async{
+  var t = await d.title.translate(to: "fr");
+  var q = await d.question.translate(to: "fr");
+  var a = await d.answer.translate(to: "fr");
+    return Devinette(t.text, q.text, a.text);
+  }
 }
--- lib/modules/devinette/view.dart
+++ lib/modules/devinette/view.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:async_button_builder/async_button_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/components/button/gf_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intellibox/modules/apis/ninja.dart';
 import 'package:intellibox/modules/devinette/model.dart';
 import 'package:intellibox/utils/theme.dart';
-import 'package:translator/translator.dart';
 
 class DevinettePage extends StatefulWidget {
   const DevinettePage({super.key});
@@ -57,29 +57,67 @@ class _DevinettePageState extends State<DevinettePage> {
                 ),
               ],
             )),
-          GFButton(
-            onPressed: () async {
-              Devinette? d = await Ninja.getDevinette(1);
-              if (d != null) {
-                devinette = Devinette(await d.title.translate(to: "fr"), question, answer)
-              }
-            },
-            text: "Générer",
-            color: Scolors.primary,
-            splashColor: Scolors.bgColor.withOpacity(0.5),
-            textStyle: GoogleFonts.aBeeZee(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Scolors.bgColor,
+          AsyncButtonBuilder(
+            child: MainButton(
+              text: 'Générer',
             ),
-            fullWidthButton: true,
-            size: 45,
+            onPressed: () async {
+              await _generate();
+            },
+            builder: (context, child, callback, buttonState) {
+              return GestureDetector(
+                onTap: callback,
+                child: child,
+              );
+            },
           ),
           SizedBox(
             height: 20,
           ),
         ],
       ),
+    );
+  }
+
+  Future<void> _generate() async {
+    Devinette? d = await Ninja.getDevinette(1);
+    if (d != null) {
+      await Devinette.translated(d).then((value) {
+        setState(() {
+          devinette = value;
+        });
+      });
+    }
+  }
+}
+
+class MainButton extends StatelessWidget {
+  const MainButton({
+    super.key,
+    required this.text,
+    this.icon,
+    this.onPressed,
+  });
+
+  final String text;
+  final Widget? icon;
+  final Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GFButton(
+      icon: icon,
+      onPressed: onPressed,
+      text: text,
+      color: Scolors.primary,
+      splashColor: Scolors.bgColor.withOpacity(0.5),
+      textStyle: GoogleFonts.aBeeZee(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Scolors.bgColor,
+      ),
+      fullWidthButton: true,
+      size: 45,
     );
   }
 }
